60c8aadd4504278f101add0270ff8885cf628b38
Mary <marieshix98@gmail.com>
2022-11-23 22:58:43 +0300

    Add dotenv file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,7 +10,7 @@ interface Props {
 
 const apiKey = "abcdef";
 // const { SG_API_KEY, FROM_EAMIL, TO_EMAIL } = process.env;
-sgMail.setApiKey(apiKey);
+sgMail.setApiKey(process.env.SG_API_KEY);
 
 const handleContact = async (
   req: NextApiRequest,
